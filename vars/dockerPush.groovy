a7abf9bb58f92a8767d08322356796a220b47107
SUNIT KHATUA <89977374+sunitkhatua@users.noreply.github.com>
2025-08-19 21:24:35 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,8 @@
-def dockerPush(String Image, String imageTag, Sting dockerHubUser){
-  withCredentials([usernamePassword(credentialsId:"dockerHubCred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
-                sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-  }  
-    sh "docker push ${dockerHubUser}/${Image}:${imageTag}"
+def dockerPush(String imageName, String imageTag, String dockerHubUserName) {
+    withCredentials([usernamePassword(credentialsId: "dockerHubCred", 
+                                      passwordVariable: "dockerHubPass", 
+                                      usernameVariable: "dockerHubUser")]) {
+        sh "echo ${dockerHubPass} | docker login -u ${dockerHubUser} --password-stdin"
+        sh "docker push ${dockerHubUser}/${imageName}:${imageTag}"
+    }
 }
